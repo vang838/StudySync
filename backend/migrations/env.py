492a1e831b4d26889beb70a33face9fda688530c
vang838 <vang838>
2026-09-25 02:37:59 -0500

    fix: removed unused import

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -8,8 +8,6 @@ from alembic import context
 from src.core.config import settings
 from src.db.base import Base
 
-import src.db.models
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
